deaeac2c2cce92196e7471f0583c98b6d21f5f1c
Max Drojjin <max.drojjin@gmail.com>
2017-09-16 13:48:36 -0400

    Added activities

---
+++ app/src/main/java/com/example/drmax/diputs/Contribute.java
@@ -0,0 +1,13 @@
+package com.example.drmax.diputs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Contribute extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contribute);
+    }
+}
--- app/src/main/java/com/example/drmax/diputs/MainActivity.java
+++ app/src/main/java/com/example/drmax/diputs/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.drmax.diputs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickHelpButton(View view) {
+        //TextView exampleTextView = (TextView) findViewById(R.id.text);
+        //exampleTextView.setText("What's yo prob yo?");
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+        Intent intent = new Intent(this, Problem.class);
+        startActivity(intent);
     }
 }
