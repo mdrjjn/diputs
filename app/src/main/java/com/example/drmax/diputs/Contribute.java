2a112eb19796798816437dea56dbe65c26247285
Max Drojjin <max.drojjin@gmail.com>
2017-09-16 19:40:27 -0400

    basic fb functionality

--- app/src/main/java/com/example/drmax/diputs/Contribute.java
+++ app/src/main/java/com/example/drmax/diputs/Contribute.java
@@ -2,12 +2,24 @@ package com.example.drmax.diputs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Contribute extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contribute);
+
+        FirebaseDatabase fbDb = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = fbDb.getReference("/message");
+        Log.d("TAG", myRef.toString());
+        myRef.setValue("wasup");
     }
+
+
 }
--- app/src/main/java/com/example/drmax/diputs/Problem.java
+++ app/src/main/java/com/example/drmax/diputs/Problem.java
@@ -2,12 +2,56 @@ package com.example.drmax.diputs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Problem extends AppCompatActivity {
+
+    public static final String TAG = "Problem";
+    private TextView answerDisplay;
+    private Button btnAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem);
+        answerDisplay = (TextView)findViewById(R.id.answerDisplay);
+        btnAnswer = (Button)findViewById(R.id.btnAnswer);
+        btnAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        FirebaseDatabase fbDb = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = fbDb.getReference("/message");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                answerDisplay.setText("That is because IU " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
+
+
 }
